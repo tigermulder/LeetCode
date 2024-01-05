154aed5da0de9e657f03edb0c0d4137ee233846c
weekendtiger <97021279+tigermulder@users.noreply.github.com>
2024-01-05 15:42:30 +0900

    Time: 952 ms (47.72%), Space: 128.1 MB (68.50%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.js
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.js
@@ -2,44 +2,103 @@
  * @param {number[][]} points
  * @return {number}
  */
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  insert(value) {
+    this.heap.push(value);
+    this.heapifyUp();
+  }
+
+  removePop() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    if (this.heap.length === 1) {
+      return this.heap.pop();
+    }
+    const root = this.heap[0];
+    const lastElement = this.heap.pop();
+    this.heap[0] = lastElement;
+    this.heapifyDown();
+    return root;
+  }
+
+  isEmpty() {
+    return this.heap.length === 0;
+  }
+
+  heapifyUp() {
+    let index = this.heap.length - 1;
+    while (index > 0) {
+      const element = this.heap[index];
+      const parentIndex = Math.floor((index - 1) / 2);
+      const parent = this.heap[parentIndex];
+
+      if (element[1] >= parent[1]) break;
+
+      this.heap[index] = parent;
+      this.heap[parentIndex] = element;
+      index = parentIndex;
+    }
+  }
+
+  heapifyDown() {
+    let index = 0;
+    const length = this.heap.length;
+
+    while (true) {
+      const leftChildIndex = 2 * index + 1;
+      const rightChildIndex = 2 * index + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIndex < length) {
+        leftChild = this.heap[leftChildIndex];
+        if (leftChild[1] < this.heap[index][1]) {
+          swap = leftChildIndex;
+        }
+      }
+
+      if (rightChildIndex < length) {
+        rightChild = this.heap[rightChildIndex];
+        if (
+          (swap === null && rightChild[1] < this.heap[index][1]) ||
+          (swap !== null && rightChild[1] < leftChild[1])
+        ) {
+          swap = rightChildIndex;
+        }
+      }
+
+      if (swap === null) break;
+
+      const temp = this.heap[index];
+      this.heap[index] = this.heap[swap];
+      this.heap[swap] = temp;
+      index = swap;
+    }
+  }
+}
+
 var minCostConnectPoints = function(points) {
-    let graph = []
+  let priorityqueue = new MinHeap()
+  let visitTable = {}
+  let startNode = 0
+  priorityqueue.insert([startNode,0])
+  let result = 0
+  while(!priorityqueue.isEmpty()){
+    let [node,cost] = priorityqueue.removePop()
+    if (visitTable[node]) continue;
+    result += cost;
+    visitTable[node] = true;
     for (let i = 0; i < points.length; i++) {
-        let [x1,y1] = points[i]
-        for (let j = i+1; j < points.length; j++) {
-            let [x2,y2] = points[j]
-            let cost = Math.abs(x2-x1)+Math.abs(y2-y1)
-            graph.push([i,j,cost])
-        }
+      if(!visitTable[i]){
+        let cost = Math.abs(points[node][0] - points[i][0]) + Math.abs(points[node][1] - points[i][1])
+        priorityqueue.insert([i, cost])
+      }
     }
-    graph.sort((a,b)=>a[2]-b[2])
-    let parent = new Array(points.length)
-    for (let i = 0; i < points.length; i++) {
-        parent[i] = i
-    }
-    function find(x){
-        if(parent[x] === x){
-            return x
-        }else{
-            return parent[x] = find(parent[x])
-        }
-    }
-    function union(x,y){
-        x = find(x)
-        y = find(y)
-        if(x < y){
-            parent[y] = x
-        }else{
-            parent[x] = y
-        }
-    }
-    let answer = 0
-    for (const [a,b,w] of graph) {
-        if(find(a) !== find(b)){
-            union(a,b)
-            answer += w
-        }
-    }
-    return answer
-    
+  }
+  return result;
 };
