fe5eb155458bf1d797cd128bbcfc3fd84219622b
weekendtiger <97021279+tigermulder@users.noreply.github.com>
2024-03-01 23:02:43 +0900

    Task Score: 100% | Correctness Score: 100%

--- BinaryGap/BinaryGap.js
+++ BinaryGap/BinaryGap.js
@@ -2,15 +2,23 @@
 // console.log('this is a debug message');
 
 function solution(n) {
-  let binary = n.toString(2)
-  let num = binary.match(/1/g).length; 
-  if(num === 1){
-    return 0
-  }else{
-    binary = binary.split('1')
-    for (let i = 0; i < binary.length; i++) {
-      binary[i] = binary[i].length
+    let binary = n.toString(2);
+    let maxGapLength = 0;
+    let currentGapLength = 0;
+    let inGap = false;
+    
+    for (let bit of binary) {
+        if (bit === '1') {
+            if (inGap) {
+                maxGapLength = Math.max(maxGapLength, currentGapLength);
+                currentGapLength = 0;
+            } else {
+                inGap = true;
+            }
+        } else if (inGap) {
+            currentGapLength++;
+        }
     }
-    return Math.max(...binary)
-  }
+    
+    return maxGapLength;
 }
